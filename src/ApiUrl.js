3979ca2a87c804a30162b2f2efdc260ee172cf50
Harryexe74 <junaidmahmood141@gmail.com>
2024-10-01 15:28:27 +0500

    Update ApiUrl.js

--- src/ApiUrl.js
+++ src/ApiUrl.js
@@ -1,4 +1,4 @@
- const ApiUrl = 'solarbackendupdated.vercel.app';
+ const ApiUrl = 'solarbackendupdated-dv2l3nz4s-harryexe74s-projects.vercel.app';
 // const ApiUrl = 'http://localhost:3000';
 
 export default ApiUrl;
